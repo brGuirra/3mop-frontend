a6215e82de8ca7601623d5f3c2cf24132c78edbb
brGuirra <br.guirra@gmail.com>
2023-11-25 10:46:54 -0300

    feat: add error messages for new contact form

--- src/components/atoms/input/index.tsx
+++ src/components/atoms/input/index.tsx
@@ -1,5 +1,22 @@
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+type InputProps = {
+  errorMessage?: string | false;
+} & React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 
-export const Input = ({ className, ...rest }: InputProps) => {
-  return <input className={`input input-bordered ${className}`} {...rest} />;
+export const Input = ({ className, errorMessage, ...rest }: InputProps) => {
+  return (
+    <>
+      <input
+        className={`input input-bordered ${className} ${
+          errorMessage && "input-error"
+        }`}
+        {...rest}
+      />
+      {errorMessage && (
+        <span className="text-error block mt-2">{errorMessage}</span>
+      )}
+    </>
+  );
 };
--- src/components/molecules/new-contact-form/index.tsx
+++ src/components/molecules/new-contact-form/index.tsx
@@ -3,40 +3,53 @@ import { useFormik } from "formik";
 import { object, string, InferType } from "yup";
 
 const newContactSchema = object({
-  firstName: string().required(),
-  lastName: string().required(),
-  email: string().email(),
-  phone: string().matches(/\d{11}/),
-  zipCode: string().matches(/d{8}/),
-  street: string().required(),
-  streetAddress: string().required(),
-  number: string().required(),
-  city: string().required(),
-  state: string().required(),
+  firstName: string().required("Nome é um campo obrigatório"),
+  lastName: string().required("Sobrenome é um campo obrigatório"),
+  email: string()
+    .email("O valor informado não é um e-mail válido")
+    .required("E-mail é um campo obrigatório"),
+  phone: string()
+    .matches(/\d{11}/, "Informar apenas números")
+    .required("Celular é um campo obrigatório"),
+  zipCode: string()
+    .matches(/\d{8}/, "Informar apenas números")
+    .required("Cep é um campo obrigatório"),
+  street: string().required("Rua é um campo obrigatório"),
+  streetAddress: string().required("Bairro é um campo obrigatório"),
+  number: string().required("Número é um campo obrigatório"),
+  city: string().required("Cidade é um campo obrigatório"),
+  state: string().required("Estado é um campo obrigatório"),
 });
 
 type NewContactSchema = InferType<typeof newContactSchema>;
 
 export const NewContactForm = () => {
-  const { values, handleChange, handleBlur, handleSubmit, errors } =
-    useFormik<NewContactSchema>({
-      initialValues: {
-        firstName: "",
-        lastName: "",
-        email: "",
-        phone: "",
-        zipCode: "",
-        street: "",
-        streetAddress: "",
-        number: "",
-        city: "",
-        state: "",
-      },
-      onSubmit: (values) => {
-        console.log(values);
-      },
-      validationSchema: newContactSchema,
-    });
+  const {
+    values,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    errors,
+    touched,
+    isValid,
+  } = useFormik<NewContactSchema>({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      zipCode: "",
+      street: "",
+      streetAddress: "",
+      number: "",
+      city: "",
+      state: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validationSchema: newContactSchema,
+  });
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-4">
@@ -48,11 +61,11 @@ export const NewContactForm = () => {
             name="firstName"
             type="text"
             className="w-full"
+            errorMessage={touched?.firstName && errors?.firstName}
             value={values.firstName}
             onChange={handleChange}
             onBlur={handleBlur}
           />
-          {errors?.firstName ? <div>{errors.firstName}</div> : null}
         </div>
         <div className="flex-1">
           <Label text="Sobrenome:" htmlFor="lastName" />
@@ -61,6 +74,7 @@ export const NewContactForm = () => {
             name="lastName"
             type="text"
             className="w-full"
+            errorMessage={touched?.lastName && errors?.lastName}
             value={values.lastName}
             onChange={handleChange}
             onBlur={handleBlur}
@@ -75,6 +89,7 @@ export const NewContactForm = () => {
             name="email"
             type="email"
             className="w-full"
+            errorMessage={touched?.email && errors?.email}
             value={values.email}
             onChange={handleChange}
             onBlur={handleBlur}
@@ -87,6 +102,7 @@ export const NewContactForm = () => {
             name="phone"
             type="tel"
             className="w-full"
+            errorMessage={touched?.phone && errors?.phone}
             value={values.phone}
             onChange={handleChange}
             onBlur={handleBlur}
@@ -100,6 +116,7 @@ export const NewContactForm = () => {
           name="zipCode"
           type="text"
           className="w-full"
+          errorMessage={touched?.zipCode && errors?.zipCode}
           value={values.zipCode}
           onChange={handleChange}
           onBlur={handleBlur}
@@ -112,6 +129,7 @@ export const NewContactForm = () => {
           name="street"
           type="text"
           className="w-full"
+          errorMessage={touched?.street && errors?.street}
           value={values.street}
           onChange={handleChange}
           onBlur={handleBlur}
@@ -125,6 +143,7 @@ export const NewContactForm = () => {
             name="number"
             type="text"
             className="w-full"
+            errorMessage={touched?.number && errors?.number}
             value={values.number}
             onChange={handleChange}
             onBlur={handleBlur}
@@ -137,6 +156,11 @@ export const NewContactForm = () => {
             name="streetAddress"
             type="text"
             className="w-full"
+            errorMessage={
+              touched?.streetAddress && errors?.streetAddress
+                ? errors.streetAddress
+                : ""
+            }
             value={values.streetAddress}
             onChange={handleChange}
             onBlur={handleBlur}
@@ -151,6 +175,7 @@ export const NewContactForm = () => {
             name="city"
             type="text"
             className="w-full"
+            errorMessage={touched?.city && errors?.city}
             value={values.city}
             onChange={handleChange}
             onBlur={handleBlur}
@@ -163,6 +188,7 @@ export const NewContactForm = () => {
             name="state"
             type="state"
             className="w-full"
+            errorMessage={touched?.state && errors?.state}
             value={values.state}
             onChange={handleChange}
             onBlur={handleBlur}
@@ -173,6 +199,7 @@ export const NewContactForm = () => {
         type="submit"
         text="Cadastrar"
         className="btn-bloc btn-primary mt-2"
+        disabled={!isValid}
       />
     </form>
   );
